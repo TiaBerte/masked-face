7e2d1608150a258574c3fbc7d1f31ade4542fd0d
Mattia Bertè <33131887+TiaBerte@users.noreply.github.com>
2023-02-03 15:56:39 +0100

    New dataset class for inference time

--- README.md
+++
@@ -1,3 +0,0 @@
-# masked-face
-
-Neural Network for masked face recognition using the [Barlow Twins](https://arxiv.org/pdf/2103.03230.pdf) technique.  
--- dataload.py
+++ dataload.py
@@ -22,6 +22,40 @@ class MaskedFaceDataset(Dataset):
 
 
     def __len__(self):
+        pass
+
+    def __getitem__(self, index):
+        '''
+        id = self.id_list[index] #str with the name
+        dir_path = self.path + id + '/'
+        img_list = os.listdir(dir_path)
+        img_sampled = random.sample(img_list, 2)
+        img_1 = read_image(dir_path + img_sampled[0]).float()/255
+        img_2 = read_image(dir_path + img_sampled[1]).float()/255
+
+
+        return self.transformation(img_1), self.transformation(img_2)
+        '''
+        pass
+
+
+    def transformation(self, img):
+        
+        normalize = transforms.Normalize(self.mean, self.std)
+        resize = transforms.Resize((self.height, self.width))
+        transform = transforms.Compose([normalize, resize])
+
+        return transform(img)
+
+
+
+class MaskedFaceDatasetTraining(MaskedFaceDataset):
+
+    def __init__(self, path, height=244, width=244):
+        super.__init__(path, height, width)
+
+
+    def __len__(self):
         return len(self.id_list)
 
 
@@ -34,16 +68,31 @@ class MaskedFaceDataset(Dataset):
         img_1 = read_image(dir_path + img_sampled[0]).float()/255
         img_2 = read_image(dir_path + img_sampled[1]).float()/255
 
-
         return self.transformation(img_1), self.transformation(img_2)
 
 
-    def transformation(self, img):
-        
-        normalize = transforms.Normalize(self.mean, self.std)
-        resize = transforms.Resize((self.height, self.width))
-        transform = transforms.Compose([normalize, resize])
+class MaskedFaceDatasetInference(MaskedFaceDataset):
+    
+    def __init__(self, path, height, width):
+        super.__init__(path, height, width)
 
-        return transform(img)
+        self.img_list = []
+        self.label = []
+        for id in self.id_list:
+            dir_path = self.path + id + '/'
+            imgs = os.listdir(dir_path)
+            for img_name in imgs:
+              self.img_list.append(dir_path + img_name)
+              self.label.append(id)
 
 
+    def __len__(self):
+        return len(self.label)
+
+
+    def __getitem__(self, index):
+
+        id = self.label[index] #str with the name
+        img = read_image(self.img_list[index]).float()/255
+
+        return self.transformation(img), id
